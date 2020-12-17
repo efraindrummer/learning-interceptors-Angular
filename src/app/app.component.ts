08d0a5ee572105b0503e39952affa1dc16ecd804
Efrain May <wwifihack@gmail.com>
2020-12-16 21:34:22 -0600

    learning interceptors

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { UsuariosService } from './services/usuarios.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'interceptorApp';
+  
+  constructor(private usuariosServices: UsuariosService){
+    this.usuariosServices.obtenerUsuarios().subscribe(resp => {
+      console.log(resp);
+    }, (err) => {
+      console.log('Error en el app component');
+    })
+  }
 }
---
+++ src/app/services/usuarios.service.ts
@@ -0,0 +1,20 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+@Injectable({
+  providedIn: 'root'
+})
+export class UsuariosService {
+
+  constructor(private http: HttpClient) { }
+
+  obtenerUsuarios(){
+    let params = new HttpParams().append('page', '2');
+    params = params.append('nombre','Efrain May');
+
+    return this.http.get(`https://reqres.in/api/users`, {
+      params,
+    }).pipe(
+      map(resp => resp['data']),
+    );
+  }
+}
